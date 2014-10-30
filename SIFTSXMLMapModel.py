28d54becc821115eaaf58f918d2ea07da3457cea
ajing <ajingnk@gmail.com>
2014-10-29 22:35:33 -0400

    for distance calculation

--- SIFTSXMLMapModel.py
+++ SIFTSXMLMapModel.py
@@ -2,6 +2,31 @@
     Map PDBe residue to SIFTS residue
 '''
 
+class UniProtInfo:
+    def __init__(self, accid, resname, resnum):
+        self.accid = accid
+        self.resname = resname
+        self.resnum  = resnum
+
+    def getAccid():
+        return self.accid
+
+    def getResName():
+        return self.resname
+
+    def getResNum():
+        return self.resnum
+
+    def __cmp__(self, uniprot):
+        if isinstance(uniprot, UniProtInfo):
+            if uniprot.getAccid() == self.accid:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
 class Residue:
     def __init__(self, resnum, resnam, reschain):
         # residue name and number from PDBe
@@ -19,13 +44,20 @@ class Residue:
         return self.resChain
 
     def setUniProtInfo(self, accid, resnum, resname):
-        self.uniprot = {"accid": accid, "residueName:": resname, "ResidueNum:": resnum}
+        self.uniprot = UniProtInfo(accid, resname, resnum)
 
     def getUniProtInfo(self):
         try:
             self.uniprot
         except:
             raise Exception("cannot find uniprot")
+
+    def getSeqDistance(self, res2):
+        if self.uniprot == res2.uniprot:
+            return self.uniprot.getResNum() - res2.uniprot.getResNum()
+        else:
+            return False
+
 
     def __str__(self):
         return "%s,%s;" %(self.resName, self.resNum)
@@ -42,6 +74,9 @@ class Protein:
         if not isinstance(residue, Residue):
             raise TypeError("wrong type for residue")
         self.resList.append(residue)
+
+    def getResidues():
+        return self.resList
 
     def iterateResidue(self):
         return iter(self.resList)
