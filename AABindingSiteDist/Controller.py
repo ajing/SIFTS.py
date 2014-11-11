55f1d71dc72b8eb44db3c186e02c0a13b3ad60a3
ajing <ajingnk@gmail.com>
2014-11-11 11:41:29 -0500

    move file around

---
+++ AABindingSiteDist/Controller.py
@@ -0,0 +1,7 @@
+'''
+    Calculate the distance between binding site to other residues
+'''
+
+def GetAllDist(bslist):
+    for residue in protein:
+
---
+++ AABindingSiteDist/Model.py
@@ -0,0 +1,6 @@
+
+import os
+# ugly programming, but just for short term use
+
+class BindingSite:
+    def __init__(self, pdbid, chainid, resnum, lig)
