e78d37935ae0d8937191f9140edd454fa41b3225
ajing <ajingnk@gmail.com>
2014-11-12 11:30:19 -0500

    more analysis code

--- analysis.R
+++ analysis.R
@@ -1,8 +1,18 @@
-
+filename = "pair.txt"
 xy = read.table(filename, sep = "\t")
 x  = xy[,1]
 y  = xy[,2]
 
+install.packages("hexbin")
+
 library(hexbin)
-hbo <- hexbin(x,y)
+hbo <- hexbin(x,y, xlab = "spatial distance", ylab = "sequence distance")
 plot(hbo)
+jpeg("hbo.jpg")
+plot(hbo)
+dev.off()
+
+content <- data.frame
+content <- read.table("order_join.txt", sep="\t", quote = "")
+pie(content[,2], labels = content[,1])
+
