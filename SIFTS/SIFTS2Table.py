5b8d2906128aad82fdf59df119efa69c62e16f7e
ajing <ajingnk@gmail.com>
2015-01-24 17:32:28 -0500

    add multiprocessing for SIFTS file convertion

--- SIFTS/SIFTS2Table.py
+++ SIFTS/SIFTS2Table.py
@@ -9,27 +9,43 @@ import os
 ## parameters
 OUTFILE = "./SIFTS/sift_sql_table.txt"
 
-def oneXML2Table(filename, fileobj):
+from multiprocessing import Pool
+import threading
+# thread safe for writing file
+mutex_writefile = threading.Lock()
+
+def oneXML2Table(filename):
+    print "filename", filename
     content = []
     protein = processOneXML(filename)
     pdbid   = protein.pdbid
-    print pdbid
     for eachres in protein.getResidues():
         uniprot = eachres.uniprot
         if uniprot is None:
             continue
         line    = "\t".join(map(str, [pdbid, eachres.resChain, eachres.resNum, eachres.resName, uniprot.accid, uniprot.resname, uniprot.resnum]))
         content.append(line)
-    fileobj.write("\n".join(content) + "\n")
+    if content:
+        mutex_writefile.acquire()
+        fileobj = open(OUTFILE, "a")
+        fileobj.write("\n".join(content) + "\n")
+        fileobj.close()
+        mutex_writefile.release()
 
 def XML2Table(filedir):
     allcontent = []
-    outobj = open(OUTFILE, "w")
+    inputlist = []
+    try:
+        os.remove(OUTFILE)
+    except:
+        pass
     for root, dirs, files in os.walk(filedir):
         for afile in files:
             afile = os.path.join(root, afile)
-            oneXML2Table(afile, outobj)
-    outobj.close()
+            inputlist.append(afile)
+    pool = Pool(processes = 7)
+    result = pool.map_async(oneXML2Table, inputlist)
+    resulttxt = result.wait()
 
 if __name__ == "__main__":
     XML2Table(XMLDIR)
