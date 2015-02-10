eb8c50b2b219508084538252136e131ba986c149
ajing <ajingnk@gmail.com>
2015-02-10 12:04:23 -0500

    test program for dssp

---
+++ PDBInfo/DSSPTest.py
@@ -0,0 +1,22 @@
+'''
+   Just for testing
+'''
+
+from Bio.PDB import PDBParser
+from Bio.PDB import DSSP
+
+def TestDSSP(model, pdbname):
+    p = PDBParser()
+    dssp = DSSP(model, pdbname)
+    for residue in dssp:
+        resinfo = residue[0]
+        second_str = residue[1]
+        ssa     = residue[2]
+        rsa     = residue[3]
+
+def main():
+    model = p.get_structure("10GS", "pdb10gs.ent")[0]
+    TestDSSP(model, "10GS")
+
+if __name__ == "__main__":
+    TestDSSP()
--- PDBInfo/GenPDBInfo.py
+++ PDBInfo/GenPDBInfo.py
@@ -1,6 +1,14 @@
 '''
     Generate PDB information for each biounit file
 '''
+def runDSSP(model, pdbname):
+    p = PDBParser()
+    dssp = DSSP(model, pdbname)
+    for residue in dssp:
+        resinfo = residue[0]
+        second_str = residue[1]
+        ssa     = residue[2]
+        rsa     = residue[3]
 
 
 def EachBioUnit(biounit)
